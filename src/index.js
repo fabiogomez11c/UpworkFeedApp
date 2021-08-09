ec071af84c6f19e14fd5b90ecaa0395a64f896a3
Fabio Gomez <fabiogomez11c@gmail.com>
2021-08-09 12:33:35 -0500

    Sort and cleaning the upwork response.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import "./styles/style.scss"
-import "./js/upwork"
+import { CandidateList } from "./js/upwork"
 import "./js/vanilla-tilt"
 
 VanillaTilt.init(document.querySelectorAll(".card"), {
     glare: true
 })
+
+const testList = new CandidateList();
+testList.fetchJobs()
--- src/js/upwork.js
+++ src/js/upwork.js
@@ -1,18 +1,23 @@
 
-// const urlUpwork = "https://cors-anywhere.herokuapp.com/https://www.upwork.com/ab/feed/topics/rss?securityToken=2d23f08bbd9a7ab37af8f9a3fd88f662218772c7473006ba84fc5340c4f4f8afc40b3d5b5a85561958a4f51866316e22126fd7a65a1e5a3bd2a7eed1e7431675&userUid=1099141625217413120&orgUid=1099141625221607425&topic=5306974"
+// const urlUpwork = "https://cors-anywhere.herokuapp.com/https://www.upwork.com/jobs/PERN-Stack-App_~015db5456a0df705f7?source=rss";
 
 // const parser = new DOMParser();
 // let res;
+// let ress;
 
 // fetch(urlUpwork).then(response => response.text())
-//     .then(str => new DOMParser().parseFromString(str, "text/xml"))
+//     .then(str => new DOMParser().parseFromString(str, "text/html"))
 //     .then(data => {
-//         res = data.querySelector("item")
-//         console.log(res)
-//         console.log(parser.parseFromString(res.querySelector("description").textContent, 'text/html'))
+//         console.log(data)
+//         // res = data.querySelector("item")
+//         // // console.log(res)
+//         // ress = parser.parseFromString(res.querySelector("description").textContent, 'text/html')
+//         // console.log(
+//         //     ress.querySelector("body").textContent
+//         // )
 //     })
 
-class CandidateList {
+export class CandidateList {
 
     constructor () {
         this.list    = {
@@ -25,20 +30,77 @@ class CandidateList {
         };
 
         this.jobList   = [];
-        this.upworkUrl = null;
+        this.topJobs   = []
+        // this.upworkUrl = "https://cors-anywhere.herokuapp.com/https://www.upwork.com/ab/feed/topics/rss?securityToken=2d23f08bbd9a7ab37af8f9a3fd88f662218772c7473006ba84fc5340c4f4f8afc40b3d5b5a85561958a4f51866316e22126fd7a65a1e5a3bd2a7eed1e7431675&userUid=1099141625217413120&orgUid=1099141625221607425&topic=5306974"
+        this.upworkUrl = "https://www.upwork.com/ab/feed/topics/rss?securityToken=2d23f08bbd9a7ab37af8f9a3fd88f662218772c7473006ba84fc5340c4f4f8afc40b3d5b5a85561958a4f51866316e22126fd7a65a1e5a3bd2a7eed1e7431675&userUid=1099141625217413120&orgUid=1099141625221607425&topic=5306974"
+
+        this.keywords = ["javascript", "html"]
     }
 
     fetchJobs (){
         /*
         method to fetch all the jobs from the feed
         */
+        
+        const parser = new DOMParser();
+
+        fetch(this.upworkUrl)
+            .then(response => response.text())
+            .then(str => parser.parseFromString(str, "text/xml"))
+            .then(elemnts => elemnts.querySelectorAll("item"))
+            .then(arr => {
+                arr.forEach(itm => {
+                    const tempArr = {
+                        title    : "",
+                        price    : "",
+                        postedOn : "",
+                        link     : "",
+                        content  : "",
+                    };
+
+                    // console.log(itm)
+                    tempArr.title   = itm.querySelector("title").textContent;
+                    tempArr.link    = itm.querySelector("guid").textContent;
+
+                    tempArr.content = parser.parseFromString(
+                        itm.querySelector("description").textContent, 'text/html'
+                    ).querySelector("body").innerHTML
+
+                    const tempContent = tempArr.content.split("<br>")
+
+                    for (let i of tempContent){
+                        if (i.includes("<b>Budget</b>") | i.includes("<b>Hourly Range</b>")){
+                            tempArr.price = i.replace("\n", "");
+                        } else if(i.includes("<b>Posted On</b>")) {
+                            tempArr.postedOn = i.replace("\n", "");
+                        }
+                    }
+
+                    const preCandidate = new Candidate(
+                        tempArr.title,
+                        tempArr.price,
+                        tempArr.postedOn,
+                        tempArr.link,
+                        tempArr.content
+                    )
+                    preCandidate.computeKeywords(this.keywords)
+
+                    this.jobList.push(preCandidate);
+                })
+                // console.log(listItems)
+            }).finally(() => {
+                // console.log(this.jobList)
+                this.sortJobs()
+            })
     }
 
     sortJobs (){
         /*
         method to sort all the jobs according to the parameters
         */
-        this.jobList = []
+
+        this.jobList.sort((a, b) => b.totalScore - a.totalScore)
+        this.topJobs = this.jobList.slice(0, 6)
     }
 
     compareCreateReplace(){
@@ -78,20 +140,27 @@ class CandidateList {
 
 class Candidate {
 
-    constructor(title, description, link){
-        this.title = title;
-        this.link  = link;
+    constructor(title, price, postedOn, link, content){
+        this.title    = title;
+        this.price    = price
+        this.postedOn = postedOn
+        this.link     = link;
+        this.content  = content
 
-        this.cleanDescription(description);
-    }
-
-    cleanDescription (description){
-        this.description = null;
     }
 
     computeKeywords(keywords){
-        this.keywordsCount = {};
+        this.keywordsCount = new Object();
         this.totalScore    = 0;
+
+        // console.log(this.content)
+        for (let i of keywords){
+            const regExpTemp = new RegExp(i, "g");
+            this.keywordsCount[i] = (this.content.toLowerCase().
+                match(regExpTemp) || []).length;
+            this.totalScore = this.totalScore + this.keywordsCount[i]
+        }
+
     }
 
     createHTML (){
